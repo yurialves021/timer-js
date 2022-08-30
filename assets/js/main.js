a2d495bfd4f5c1700a70ac17b1b68b7823b35827
Yuri Alves <yurialvesedf@gmail.com>
2022-08-30 14:13:55 -0300

    Criando funções

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+const relogio = document.querySelector('.relogio')
+
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+
+let segundos = 0
+let timer
+
+
+iniciar.addEventListener('click', function (e) {
+    clearInterval(timer)
+    iniciaRelogio()
+
+})
+
+pausar.addEventListener('click', function (e) {
+    clearInterval(timer)
+})
+
+zerar.addEventListener('click', function (e) {
+    clearInterval(timer)
+    relogio.innerHTML = "00:00:00"
+    segundos = 0
+})
+
+function criarSegundos(segundos) {
+    let data = new Date(segundos * 1000)
+
+    return data.toLocaleTimeString('pt-BR', {
+        timeZone: 'UTC',
+        hour12: false
+    })
+}
+
+function iniciaRelogio() {
+    timer = setInterval(function () {
+        segundos++
+        relogio.innerHTML = criarSegundos(segundos)
+    }, 1000)
+}
